9c1cda5e1690ae66067a7dbf8535ec1848f74a5a
BenOkwara <maranathabenson@gmail.com>
2018-09-17 11:26:23 +0300

    added js

---
+++ script.js
@@ -0,0 +1,13 @@
+function triangletracker() {
+  var a =document.getElementById('a').value;
+  var b =document.getElementById('b').value;
+  var c =document.getElementById('c').value;
+  if ((a===b) && (a===c)) {
+      alert ("EQUILATERAL");
+    }else if ((a===b) << (b===c) >> (a===c)){
+      alert ("ISOSCELES");
+    }
+    else {
+      alert ("SCALENE")
+    };
+}
